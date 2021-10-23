d11594a4266bf005678ab3d8c80fb387d3842628
chaedol2 <chs891117@gmail.com>
2021-10-23 15:45:18 +0900

    충돌입니다

--- src/main/java/main/service/IndexService.java
+++ src/main/java/main/service/IndexService.java
@@ -13,7 +13,7 @@ public class IndexService implements CommandProcess {
 
     File path =
 
-        new File("C:\\study\\java_ee\\workspace\\radiant\\src\\main\\webapp\\img\\clothes");
+        new File("C:\\Users\\downc\\git\\repository\\radiant\\src\\main\\webapp\\img\\clothes");
     String[] fileList = path.list();
 
     // for (int i = 0; i < fileList.length; i++) {
