611f8ff966e0a2541088373e142711ac5e02702a
zzz <znzl9dnjwnj@gmail.com>
2021-10-30 23:57:58 +0900

    상세페이지 중복제거

--- src/main/java/staticFile/StaticFile.java
+++ src/main/java/staticFile/StaticFile.java
@@ -5,8 +5,8 @@ import java.io.File;
 public class StaticFile {
 	
 	//public static File path = new File("C:\\Users\\downc\\git\\repository\\radiant\\src\\main\\webapp\\img\\clothes"); //현석님
-	public static File path = new File("C:\\study\\java_ee\\workspace\\radiant\\src\\main\\webapp\\img\\clothes");//아라언니
-	//public static File path = new File("C:\\java__ee\\workspace\\radiant\\src\\main\\webapp\\img\\clothes");
+	//public static File path = new File("C:\\study\\java_ee\\workspace\\radiant\\src\\main\\webapp\\img\\clothes");//아라언니
+	public static File path = new File("C:\\java__ee\\workspace\\radiant\\src\\main\\webapp\\img\\clothes");
 	//public static File path = new File("D:/java_ee/workspace/radiant/src/main/webapp/img/clothes");//현정
 	
 }
